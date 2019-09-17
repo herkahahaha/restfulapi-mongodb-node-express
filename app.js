a5f694a15ef9e349e51454f81bd8e57e0c8aa28a
Herkahahaha <herkahahaha@gmail.com>
2019-09-18 03:52:53 +0700

    add orders

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ const app = express();
 const productRoutes = require("./api/routes/products");
 app.use("/products", productRoutes);
 
+// import routes from api folder
+const ordersRoutes = require("./api/routes/orders");
+app.use("/orders", ordersRoutes);
+
 module.exports = app;
