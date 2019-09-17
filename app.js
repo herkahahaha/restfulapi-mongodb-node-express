5049b4e99df1e0853acc2581b8dd03d2b9edeec8
Herkahahaha <herkahahaha@gmail.com>
2019-09-18 04:04:09 +0700

    add handling error

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 
+// handling project setup
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
 // import routes from api folder
 const productRoutes = require("./api/routes/products");
 app.use("/products", productRoutes);
 
-// import routes from api folder
 const ordersRoutes = require("./api/routes/orders");
 app.use("/orders", ordersRoutes);
+
+/* ------------------
+    Handling Error 
+---------------------*/
+app.use((req, res, next) => {
+  const error = new Error("not found");
+  error.status(404);
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: { message: "error message" } });
+});
 
 module.exports = app;
