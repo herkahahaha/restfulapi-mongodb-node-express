40a1cab4042768695c331288eb63809ed29f77b9
Herkahahaha <herkahahaha@gmail.com>
2019-09-19 11:31:29 +0700

    add user login and jwt signing

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 // Signup for new users
 router.post("/signup", (req, res, next) => {
@@ -37,6 +38,43 @@ router.post("/signup", (req, res, next) => {
           }
         });
       }
+    });
+});
+
+// Login using auth by jsonwebtoken
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (error, result) => {
+        if (error) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            "secretkeyusingenvfor development",
+            { expiresIn: "1h" }
+          );
+          return res.status(200).json({
+            message: "Auth Success",
+            token: token
+          });
+        }
+        res.status(401).json({
+          message: "Auth Failed"
+        });
+      });
     });
 });
 
